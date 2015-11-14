8f347fcb5df9ea0f9ef65ed877f81888328dbb1e
Nathan Hicks <nathan.m.hicks@gmail.com>
2015-11-14 16:09:28 -0500

    Complete on exercise3-3.c except for a-b-c cases

--- chapter3/exercise3-3.c
+++ chapter3/exercise3-3.c
@@ -7,29 +7,42 @@ literally.
 */
 
 #include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 
 void expand(char s1[],char s2[]);
 
 int main(void)
 {
-    char s1[] = "a-z,A-Z0-9";
+    char s1[] = "-a-z,A-Z0-9-a-z0-9a-b-c";
     char s2[1000];
     expand(s1,s2);
-    printf("%s\n",s2);
     return 0;
 }
 
 void expand(char s1[], char s2[])
 {
-    int i,j; 
-    for(i = 0,j = 97; j <= 122; i++,j++) {
-        s2[i] = j;
+    int i,j,k;
+    char c1,c2,c3;
+
+    while(k < strlen(s1)) {
+      /* The following 3 if statements are looking for an a-z,A-Z,or 0-9 pattern */
+      c1 = s1[k];
+      if (isdigit(c1) || isalpha(c1) || islower(c1) || isupper(c1)) {
+        c2 = s1[k+1];
+        if (c2 == '-') {
+          c3 = s1[k+2];
+          if (isdigit(c3) || isalpha(c3) || islower(c3) || isupper(c3)) {
+            for(i = 0,j = c1; j <= c3; i++,j++) {
+              s2[i] = j;
+            }
+          }
+        }
+        s2[i] = '\0';
+        printf("%s\n",s2);
+        k+=3;
+      } else {
+        k+=1;
+      }
     }
-    /*for(i = 0,j = 65; j <= 90; i++,j++) {
-        s2[i] = j;
-    }*/
-    /*for(i = 0,j = 0; j <= 9; i++,j++) {
-        s2[i] = j;
-    }*/
-    
 }
