f0d396fce7721cdf6856dbb0f7bc11f37c6ff165
Nathan Hicks <nathan.m.hicks@gmail.com>
2015-11-15 16:01:33 -0500

    initial shell for exercise4-1.c

---
+++ chapter4/exercise4-1.c
@@ -0,0 +1,19 @@
+/* Exercise 4-1.  Write the function strrindex(s,t), which returns the position
+ * of the rightmost occurrence of t in s, or -1 if there is none.
+ */
+#include<stdio.h>
+#include<string.h>
+
+int strrindex(char s[], char t[]);
+
+int main(void)
+{
+
+  return 0;
+}
+
+int strrindex(char s[],char t[])
+{
+
+  return 0;
+}
